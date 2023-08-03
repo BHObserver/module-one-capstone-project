29761224d3cb6366928f479f7f7b63d88949533a
BHObserver <BHObserver0@gmail.com>
2023-08-03 23:28:06 +0600

    Update main.js

--- main.js
+++ main.js
@@ -46,3 +46,43 @@ moreBtn.addEventListener('click', () => {
     e.style.display = 'flex';
   });
 });
+
+/* Add dynamic */
+const perfomers = [
+  {
+    image: '<img src="./images/Tamikrest.jpg" alt="performer-photo" class="performer-img">',
+    name: 'Tamikrest',
+    type: 'Mali Band',
+    intro: 'Tamikrest is a group of musicians who belong to the Tuareg people. The band was founded in 2006 in Kidal, Mali. They mix traditional African music with Western rock and pop influences and sing in Tamashek.',
+  },
+  {
+    image: '<img src="./images/AsíAsí.jpg" alt="performer-photo" class="performer-img">',
+    name: 'Así Así',
+    type: 'Mexican/American band',
+    intro: 'Así Así is a Mexican/American band formerly known as El Mañana formed in Mexico City in 2009. The band consists of Fernando de Buen (guitar/vocals), Ben Geissel (drums), Celeen Rusk (keyboards), and Sam Coplin (bass)',
+  },
+  {
+    image: '<img src="./images/CheikhIbraFam.jpg" alt="performer-photo" class="performer-img">',
+    name: 'Cheikh Ibra Fam',
+    type: 'Performer',
+    intro: 'Cheikh Ibra Fam is a multidimensional performer whose nearly 15 year career has demonstrated a free and futuristic spirit, well anchored in his Senegalese roots.',
+  },
+  {
+    image: '<img src="./images/topdonn.jpg" alt="performer-photo" class="performer-img">',
+    name: 'DJ TopDonn',
+    type: 'Disc Jockey',
+    intro: "Originally hailing from Nairobi, Kenya, DJ TopDonn's involvement on the Afro Urban music scene has seen him grace the stage in various capacities with industry heavyweights such as Mafikizolo, Koffi Olomide, Fally Ipupa, Serge Beynaud, Les Nubians, Angelique Kidjo & Yossou N'dour.",
+  },
+  {
+    image: '<img src="./images/SanSalvador.jpg" alt="performer-photo" class="performer-img">',
+    name: 'San Salvador',
+    type: 'Folk Band',
+    intro: 'Occitan polyphony is the starting point for this six-voice and percussion collective as they embark on their search for universal folklore, rooted in the region’s deep troubadour traditions yet circulating between cultures and musical genres.',
+  },
+  {
+    image: '',
+    name: '',
+    type: '',
+    intro: '',
+  },
+];
