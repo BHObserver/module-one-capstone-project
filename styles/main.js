a2b31ce3b98d18d53303ce72981c05b449add675
BHObserver <BHObserver0@gmail.com>
2023-08-03 16:41:25 +0600

    Delete main.js

--- styles/main.js
+++
@@ -1,8 +0,0 @@
-const moreBtn = document.querySelector('.moreBtn');
-const performer = document.querySelectorAll('.performer');
-console.log(performer)
-moreBtn.addEventListener('click', () => {
-  performer.forEach((e) => {
-    e.style.display = 'flex';
-  });
-});
